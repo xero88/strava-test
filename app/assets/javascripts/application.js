00430c04dd08308431e9ed37c93f2eb11e691ada
Anthony Bernardo <bernardo@icare.ch>
2018-03-27 17:43:57 +0200

    Commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,2 @@
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require foundation
-//= require simplemde.min
-//= require questions
-
-$(document).on('turbolinks:load', function() {
-  $(document).foundation();
-});
